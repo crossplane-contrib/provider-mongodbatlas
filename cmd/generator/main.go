73c11f2aa3eaa0c00ce1e88b6426d11a3e5d365f
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2021-12-21 18:48:59 +0300

    Initial commit

---
+++ cmd/generator/main.go
@@ -0,0 +1,49 @@
+// +build generate
+
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	tf "github.com/hashicorp/terraform-provider-hashicups/hashicups"
+
+	"github.com/crossplane-contrib/terrajet/pkg/pipeline"
+	// Comment out the line below instead of the above, if your Terraform
+	// provider uses an old version (<v2) of github.com/hashicorp/terraform-plugin-sdk.
+	// "github.com/crossplane-contrib/terrajet/pkg/types/conversion"
+
+	"github.com/crossplane-contrib/provider-jet-template/config"
+)
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		panic("root directory is required to be given as argument")
+	}
+	absRootDir, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		panic(fmt.Sprintf("cannot calculate the absolute path of %s", os.Args[1]))
+	}
+	resourceMap := tf.Provider().ResourcesMap
+	// Comment out the line below instead of the above, if your Terraform
+	// provider uses an old version (<v2) of github.com/hashicorp/terraform-plugin-sdk.
+	// resourceMap := conversion.GetV2ResourceMap(tf.Provider())
+	pipeline.Run(config.GetProvider(resourceMap), absRootDir)
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/crossplane-contrib/provider-jet-template
+
+go 1.16
+
+require (
+	github.com/crossplane-contrib/terrajet v0.2.1
+	github.com/crossplane/crossplane-runtime v0.15.1-0.20211004150827-579c1833b513
+	github.com/crossplane/crossplane-tools v0.0.0-20210916125540-071de511ae8e
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
+	github.com/hashicorp/terraform-provider-hashicups v0.3.1
+	github.com/pkg/errors v0.9.1
+	go.uber.org/multierr v1.7.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	k8s.io/apimachinery v0.22.0
+	k8s.io/client-go v0.22.0
+	sigs.k8s.io/controller-runtime v0.9.6
+	sigs.k8s.io/controller-tools v0.6.2
+)
+
+// Comment out the line below instead of the above, if your Terraform provider
+// uses an old version (<v2) of github.com/hashicorp/terraform-plugin-sdk.
+// replace github.com/hashicorp/terraform-plugin-sdk => github.com/turkenh/terraform-plugin-sdk v1.17.2-patch1

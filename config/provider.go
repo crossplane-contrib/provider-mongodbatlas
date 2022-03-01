e42f06c68f4a5697a373913f57046620a2f44f5b
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-03-01 19:16:15 +0300

    Fix IPAccessList resource configuration
    - Add example manifest for the IPAccessList resource
    
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
    (cherry picked from commit f6604eecd5a21529f8ece143d86e281a8fc2be34)

--- config/provider.go
+++ config/provider.go
@@ -21,6 +21,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/crossplane-contrib/provider-jet-mongodbatlas/config/mongodbatlas"
+	"github.com/crossplane-contrib/provider-jet-mongodbatlas/config/project"
 )
 
 const (
@@ -44,6 +45,7 @@ func GetProvider(resourceMap map[string]*schema.Resource) *tjconfig.Provider {
 	for _, configure := range []func(provider *tjconfig.Provider){
 		// add custom config functions
 		mongodbatlas.Configure,
+		project.Configure,
 	} {
 		configure(pc)
 	}

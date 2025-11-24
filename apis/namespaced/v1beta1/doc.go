55fefe5a2bbe1e1a70259de2d67dc5b461de63f1
Javier Gascón <javier.gascon@lansweeper.com>
2025-11-24 11:52:06 +0100

    feat(#32): Update apis
    Signed-off-by: Javier Gascón <javier.gascon@lansweeper.com>

---
+++ apis/namespaced/v1beta1/doc.go
@@ -0,0 +1,9 @@
+/*
+Copyright 2022 Upbound Inc.
+*/
+
+// Package v1beta1 contains the core resources of the provider.
+// +kubebuilder:object:generate=true
+// +groupName=mongodbatlas.m.crossplane.io
+// +versionName=v1beta1
+package v1beta1

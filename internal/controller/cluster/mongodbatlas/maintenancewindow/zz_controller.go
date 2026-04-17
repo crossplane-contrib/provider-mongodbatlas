ad1cfb79cdcaf708186e911bf3a7d3809dbe5acc
Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
2026-04-17 13:12:20 +0200

    fix: bump up go + dependencies
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>

--- internal/controller/cluster/mongodbatlas/maintenancewindow/zz_controller.go
+++ internal/controller/cluster/mongodbatlas/maintenancewindow/zz_controller.go
@@ -69,8 +69,7 @@ func Setup(mgr ctrl.Manager, o tjcontroller.Options) error {
 	// register webhooks for the kind v1alpha1.MaintenanceWindow
 	// if they're enabled.
 	if o.StartWebhooks {
-		if err := ctrl.NewWebhookManagedBy(mgr).
-			For(&v1alpha1.MaintenanceWindow{}).
+		if err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.MaintenanceWindow{}).
 			Complete(); err != nil {
 			return errors.Wrap(err, "cannot register webhook for the kind v1alpha1.MaintenanceWindow")
 		}
--- internal/controller/namespaced/mongodbatlas/flexcluster/zz_controller.go
+++ internal/controller/namespaced/mongodbatlas/flexcluster/zz_controller.go
@@ -69,8 +69,7 @@ func Setup(mgr ctrl.Manager, o tjcontroller.Options) error {
 	// register webhooks for the kind v1alpha1.FlexCluster
 	// if they're enabled.
 	if o.StartWebhooks {
-		if err := ctrl.NewWebhookManagedBy(mgr).
-			For(&v1alpha1.FlexCluster{}).
+		if err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.FlexCluster{}).
 			Complete(); err != nil {
 			return errors.Wrap(err, "cannot register webhook for the kind v1alpha1.FlexCluster")
 		}

23eef02ba66e1696b9ad09709158dfc2f4dd6cb8
Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
2026-04-17 13:18:04 +0200

    fix: lint due to runtime ugprade
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>

--- internal/controller/cluster/providerconfig/config.go
+++ internal/controller/cluster/providerconfig/config.go
@@ -28,7 +28,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		Watches(&v1beta1.ProviderConfigUsage{}, &resource.EnqueueRequestForProviderConfig{}).
 		Complete(providerconfig.NewReconciler(mgr, of,
 			providerconfig.WithLogger(o.Logger.WithValues("controller", name)),
-			providerconfig.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
+			providerconfig.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))))) //nolint: staticcheck
 }
 
 // SetupGated adds a controller that reconciles ProviderConfigs by accounting for
